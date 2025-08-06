8bfcb67ea69c0ab1c960f9d960404d0e93e72bff
bordi00 <matteo.bordignon@studenti.unitn.it>
2025-08-06 12:45:57 +0200

    feat: added NodeCommand impl

--- src/types.rs
+++ src/types.rs
@@ -15,3 +15,21 @@ pub enum NodeCommand {
     AddSender(NodeId, Sender<Packet>),
     StartFlood,
 }
+
+impl NodeCommand {
+    pub fn as_add_sender(self) -> Option<(NodeId, Sender<Packet>)> {
+        match self {
+            NodeCommand::AddSender(id, sender) => Some((id, sender)),
+            _ => None,
+        }
+    }
+
+    pub fn is_add_sender(&self) -> bool {
+        matches!(self, Self::AddSender(_, _))
+    }
+
+    pub fn is_start_flood(&self) -> bool {
+        matches!(self, Self::StartFlood)
+    }
+}
+
