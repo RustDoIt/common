b07aea9efbc43148ecf8587d81b759c3e6574039
bordi00 <matteo.bordignon@studenti.unitn.it>
2025-08-09 11:34:59 +0200

    feat: added flood request handling

--- src/routing_handler.rs
+++ src/routing_handler.rs
@@ -8,7 +8,7 @@ pub struct RoutingHandler {
     id: NodeId,
     network_view: Network,
     neighbors: HashMap<NodeId, Sender<Packet>>,
-    flood_seen: HashSet<(NodeId, u64)>,
+    flood_seen: HashSet<(u64, NodeId)>,
     session_counter: u64,
     flood_counter: u64
 }
@@ -48,7 +48,109 @@ impl RoutingHandler {
         Ok(())
     }
 
+    pub fn add_neighbor(&mut self, node_id: NodeId, sender: Sender<Packet>) -> Result<(), NetworkError> {
+        let _ = self.neighbors.insert(node_id, sender);
+        self.network_view.update_node(self.id,vec![node_id])?;
+        Ok(())
+    }
+
     pub fn handle_flood_response(&mut self, flood_response: FloodResponse) -> Result<(), NetworkError> {
-        unimplemented!()
+        if flood_response.flood_id == self.flood_counter {
+            for (i, &(node_id, node_type)) in flood_response.path_trace.iter().enumerate() {
+                let mut neighbors = Vec::new();
+
+                // Add previous node as neighbor
+                if i > 0 {
+                    neighbors.push(flood_response.path_trace[i - 1].0);
+                }
+
+                // Add next node as neighbor
+                if i + 1 < flood_response.path_trace.len() {
+                    neighbors.push(flood_response.path_trace[i + 1].0);
+                }
+
+                // Try to update existing node or add new one
+                if let Err(_) = self.network_view.update_node(node_id, neighbors.clone()) {
+                    let new_node = Node::new(node_id, node_type, neighbors.clone());
+                    self.network_view.add_node(new_node)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn handle_flood_request(&mut self, mut flood_request: FloodRequest, session_id: u64) -> Result<(), NetworkError> {
+        let prev_hop = flood_request.path_trace
+            .last()
+            .map(|x| x.0)
+            .unwrap_or(flood_request.initiator_id);
+
+        flood_request.path_trace.push((self.id, NodeType::Drone));
+
+        let flood_session = (flood_request.flood_id, flood_request.initiator_id);
+
+        if !self.flood_seen.insert(flood_session) || self.neighbors.len() == 1 {
+            // generate flood response
+
+            let route= if let Ok(path) = self.network_view.find_path(flood_request.initiator_id) {
+                SourceRoutingHeader::new(path, 1)
+            } else {
+                let mut route: Vec<_> = flood_request.path_trace
+                    .clone()
+                    .iter()
+                    .map(|(id, _)| *id)
+                    .rev()
+                    .collect::<Vec<_>>();
+
+
+                if route.last() != Some(&flood_request.initiator_id){
+                    route.push(flood_request.initiator_id);
+                }
+
+                SourceRoutingHeader::new(route, 1)
+            };
+
+            let flood_response = FloodResponse {
+                flood_id: flood_request.flood_id,
+                path_trace: flood_request.path_trace,
+            };
+
+            let packet = Packet::new_flood_response(route, session_id, flood_response);
+
+            self.send_packet_to_first_hop(packet);
+
+            return Ok(());
+        }
+
+        let srh = SourceRoutingHeader::new(vec![], 0);
+
+        let new_flood_request = Packet::new_flood_request(
+            srh,
+            session_id,
+            flood_request,
+        );
+
+        for (neighbor_id, neighbor) in self.neighbors.iter() {
+            if *neighbor_id != prev_hop {
+                // TODO: send to controller
+                neighbor.send(new_flood_request.clone()).map_err(|_| NetworkError::NodeNotFound)?
+            }
+        }
+        Ok(())
+    }
+
+      /// Send a packet to the first hop in its route
+    fn send_packet_to_first_hop(&self, packet: Packet) -> Result<(), NetworkError> {
+        if packet.routing_header.hops.len() > 1 {
+            let first_hop = packet.routing_header.hops[1];
+            if let Some(sender) = self.neighbors.get(&first_hop) {
+                sender.send(packet).map_err(|_|
+                    NetworkError::NodeNotFound)?;
+            } else {
+                return Err(NetworkError::TopologyError);
+
+            }
+        }
+        Ok(())
     }
 }
