589cb9a3851a493d878becc11c2aa27879d53427
bordi00 <matteo.bordignon@studenti.unitn.it>
2025-08-22 19:45:00 +0200

    feat: added function in Network

--- src/network.rs
+++ src/network.rs
@@ -84,7 +84,7 @@ impl std::fmt::Debug for Node{
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Network {
     pub(crate) nodes: Vec<Node>
 }
@@ -96,6 +96,17 @@ impl Network {
         Self { nodes }
     }
 
+
+    pub fn add_node_controller_view(&mut self, node_id: NodeId, node_type: NodeType, adjacents: &[NodeId]) {
+        let drone = Node::new(node_id, node_type, adjacents.to_vec());
+        for adj in adjacents {
+            if let Some(node) = self.nodes.iter_mut().find(|n| n.id == *adj){
+                node.add_adjacent(*adj);
+            }
+        }
+
+        self.nodes.push(drone)
+    }
 
     pub(crate) fn add_node(&mut self, new_node: Node) {
         for adj in new_node.get_adjacents() {
